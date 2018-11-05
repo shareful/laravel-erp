a202d679068325515512b43e3266e1df7623acbe
Shareful Islam <km.shareful@gmail.com>
2018-11-05 13:25:38 +0600

    removed unnecessary commented code

--- user/app/Http/Controllers/Api/V1/RegisterController.php
+++ user/app/Http/Controllers/Api/V1/RegisterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Models\User;
-// use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use JWTAuth;
